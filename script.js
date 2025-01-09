e90161894c75c80cd5de49720e5a8ee16d819760
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-01-09 15:42:11 +0100

    fix code

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
 const prezzoKM = 0.21;
 let prezzoFinaleBiglietto;
-let kmDaPercorrere;
-let etaPasseggero;
-kmDaPercorrere = window.prompt("Quanti km devi percorrere?");
-etaPasseggero = window.prompt("Quanti anni hai?");
+const kmDaPercorrere = window.prompt("Quanti km devi percorrere?");
+const etaPasseggero = window.prompt("Quanti anni hai?");
+let prezzo = prezzoKM * kmDaPercorrere;
 if (etaPasseggero < 18) {
-  prezzoFinaleBiglietto = (prezzoKM * kmDaPercorrere)-((prezzoKM * kmDaPercorrere) * 20/100);
+  prezzoFinaleBiglietto = prezzo - (prezzo * 20/100);
 } else if (etaPasseggero > 65) {
-  prezzoFinaleBiglietto = (prezzoKM * kmDaPercorrere)-((prezzoKM * kmDaPercorrere) * 40/100);
+  prezzoFinaleBiglietto = prezzo-(prezzo * 40/100);
 } else {
-  prezzoFinaleBiglietto = prezzoKM * kmDaPercorrere;
+  prezzoFinaleBiglietto = prezzo;
 }
 alert("Il prezzo del biglietto è di " + prezzoFinaleBiglietto.toFixed(2) + "€");
-
-
-
-
-
-
-// const prezzoKM = 0.21;
-// let prezzoFinaleBiglietto;
-// let kmDaPercorrere;
-// let etaPasseggero;
-// kmDaPercorrere = window.prompt("Quanti km devi percorrere?");
-// etaPasseggero = window.prompt("Quanti anni hai?");
-// let prezzo = prezzoKM * kmDaPercorrere;
-// if (etaPasseggero < 18) {
-//   prezzoFinaleBiglietto = (prezzo)-(prezzo) * 20/100);
-// } else if (etaPasseggero > 65) {
-//   prezzoFinaleBiglietto = (prezzoKM * kmDaPercorrere)-((prezzoKM * kmDaPercorrere) * 40/100);
-// } else {
-//   prezzoFinaleBiglietto = prezzoKM * kmDaPercorrere;
-// }
-// alert("Il prezzo del biglietto è di " + prezzoFinaleBiglietto.toFixed(2) + "€");
