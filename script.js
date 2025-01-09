c01e2c6094c5b587a715b0d16f7eb3553dd32fe3
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-01-09 13:25:09 +0100

    add input code and if

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const prezzoKM = 0.21
+const prezzoKM = 0.21;
 let prezzoFinaleBiglietto;
 let kmDaPercorrere;
 let etaPasseggero;
 
-
-
+kmDaPercorrere = window.prompt("Quanti km devi percorrere?");
+etaPasseggero = window.prompt("Quanti anni hai?");
+if (etaPasseggero < 18) {
+  prezzoFinaleBiglietto = prezzoKM * kmDaPercorrere * 0.2;
+} else if (etaPasseggero > 65) {
+  prezzoFinaleBiglietto = prezzoKM * kmDaPercorrere * 0.4;
+} else {
+  prezzoFinaleBiglietto = prezzoKM * kmDaPercorrere;
+}
